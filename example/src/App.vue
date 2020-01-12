cf9d4495e21bc039a483899cb724f67f68ab5cd6
boenfu <1997@boenfu.cn>
2020-01-12 19:56:15 +0800

    Update example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,29 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld v-veveal.fadeInUp="'fadeInDown'" msg="Welcome to Your Vue.js + TypeScript App" />
+    <img src="./assets/1.jpg" v-veveal.flash.infinite />
+    <div>
+      <img src="./assets/2.jpg" v-veveal.shake.infinite />
+      <img src="./assets/3.jpg" v-veveal.tada.d1 />
+    </div>
+
+    <main>
+      <section v-for="i in 10" :key="i" v-veveal.fadeInUp>
+        <p v-veveal.fadeInLeft.d1>
+          <a href="https://github.com/boenfu/veveal" target="_blank">veveal</a>
+        </p>
+        <img v-veveal.fadeInRight.d1 v-if="i % 2" src="./assets/4.jpg" />
+        <img v-veveal.fadeInRight.d1 v-else src="./assets/5.jpg" />
+      </section>
+    </main>
+
+    <img src="./assets/6.jpg" v-veveal.rollIn />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
+@Component({})
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 480px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+div {
+  display: flex;
+}
+
+div:not(#app) img {
+  width: 240px;
+  height: auto;
+}
+
+#app > img {
+  margin: 20px;
+}
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+section {
+  width: 48%;
+  box-sizing: border-box;
+  padding: 24px;
+  height: 200px;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  box-shadow: 2px 1px 6px #000;
+  background-color: #fff;
+}
+
+section p {
+  width: 100%;
+  text-align: left;
+  font-size: 24px;
+}
+
+section img {
+  width: 120px;
+  height: auto;
 }
 </style>
--- example/src/main.ts
+++ example/src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import plugin from "../../plugin/src/plugin";
 
-Vue.use(plugin);
+Vue.use(plugin, { distance: 200 });
 
 new Vue({
   render: h => h(App)
